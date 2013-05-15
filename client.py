ce89491b6f016f87d7a9f6bb9cff4382cdf3f5b4
shin- <joffrey@dotcloud.com>
2013-05-15 11:22:32 -0700

    Handling responses appropriately (basic error handling, JSON results, raw streams when appropriate)

--- client.py
+++ client.py
@@ -3,16 +3,20 @@ import string
 import requests
 
 class Client(requests.Session):
-    def __init__(self, base_url="http://localhost"):
+    def __init__(self, base_url="http://localhost:4243"):
+        super(Client, self).__init__()
         self.base_url = base_url
 
     def _url(self, path):
         return self.base_url + path
 
-    def _result(self, response):
+    def _result(self, response, json=False):
         # FIXME
         if response.status_code != 200:
+            print response.text
             response.raise_for_status()
+        if json:
+            return response.json()
         return response
 
     def build(self, dockerfile):
@@ -33,14 +37,14 @@ class Client(requests.Session):
     def containers(self, quiet=False, all=False, trunc=True, latest=False, since=None, before=None, limit=-1):
         params = {
             'limit': 1 if latest else limit,
-            'only_ids': quiet,
-            'all': all,
-            'trunc_cmd': trunc,
+            'only_ids': 1 if quiet else 0,
+            'all': 1 if all else 0,
+            'trunc_cmd': 1 if trunc else 0,
             'since': since,
             'before': before
         }
         u = self._url("/containers/ps")
-        return self._result(self.get(u, params=params))
+        return self._result(self.get(u, params=params), True)
 
     def create_container(self, image, command, hostname=None, user=None, detach=False,
         stdin_open=False, tty=False, mem_limit=0, ports=None, environment=None, dns=None,
@@ -70,24 +74,27 @@ class Client(requests.Session):
         return self._result(res)
 
     def diff(self, container):
-        return self._result(self.get(self._url("/containers/{0}/changes".format(container))))
+        return self._result(self.get(self._url("/containers/{0}/changes".format(container))), True)
 
     def export(self, container):
         res = self.get(self._url("/containers/{0}/export".format(container)), stream=True)
         return res.raw
 
     def history(self, image):
-        return self._result(self.get(self._url("/images/{0}/history".format(image))))
+        res = self.get(self._url("/images/{0}/history".format(image)))
+        if res.status_code == 500 and string.count(res.text, "Image does not exist") == 1:
+            raise KeyError(res.text)
+        return self._result(res)
 
     def images(self, name=None, quiet=False, all=False, viz=False):
         if viz:
             return self._result(self.get(self._url("images/viz")))
         params = {
             'filter': name,
-            'only_ids': quiet,
-            'all': all,
+            'only_ids': 1 if quiet else 0,
+            'all': 1 if all else 0,
         }
-        return self._result(self.get(self._url("/images/json"), params=params))
+        return self._result(self.get(self._url("/images/json"), params=params), True)
 
     def import_image(self, src, repository=None, tag=None):
         u = self._url("/images/create")
@@ -102,7 +109,7 @@ class Client(requests.Session):
         return self._result(self.post(u, src, params=params))
 
     def info(self):
-        return self._result(self.get(self._url("/info")))
+        return self._result(self.get(self._url("/info")), True)
 
     def insert(self, image, url, path):
         url = self._url("/images/" + image)
@@ -113,10 +120,10 @@ class Client(requests.Session):
         return self._result(self.post(url, None, params=params))
 
     def inspect_container(self, container_id):
-        return self._result(self.get(self._url("/containers/{0}/json".format(container_id))))
+        return self._result(self.get(self._url("/containers/{0}/json".format(container_id))), True)
 
     def inspect_image(self, image_id):
-        return self._result(self.get(self._url("/images/{0}/json".format(image_id))))
+        return self._result(self.get(self._url("/images/{0}/json".format(image_id))), True)
 
     def kill(self, *args):
         for name in args:
@@ -134,7 +141,7 @@ class Client(requests.Session):
             'password': password if password is not None else json['password'],
             'email': email if email is not None else json['email']
         }
-        return self._result(self.post(url, req_data))
+        return self._result(self.post(url, req_data), True)
 
     def logs(self, container):
         params = {
@@ -189,7 +196,8 @@ class Client(requests.Session):
             self.post(url, None, params=params)
 
     def search(self, term):
-        return self._result(self.get(self._url("/images/search")), params={'term': term})
+        return self._result(self.get(self._url("/images/search"), params={'term': term}),
+            True)
 
     def start(self, *args):
         for name in args:
@@ -214,7 +222,7 @@ class Client(requests.Session):
         return self._result(self.post(url, None, params=params))
 
     def version(self):
-        return self._result(self.get(self._url("/version")))
+        return self._result(self.get(self._url("/version")), True)
 
     def wait(self, *args):
         result = []
