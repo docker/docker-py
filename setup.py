b3378224b21cf22cc200d04f449e7e256e8c5294
shin- <joffrey@dotcloud.com>
2013-07-05 19:08:43 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ SOURCE_DIR = os.path.join(ROOT_DIR)
 test_requirements = []
 setup(
     name="docker-py",
-    version='0.0.5',
+    version='0.0.6',
     description="Python client for Docker.",
     packages=['docker'],
     install_requires=['requests'] + test_requirements,
