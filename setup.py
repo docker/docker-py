2108a54fcb6287113bb7ceb13e4cbbb8933bdb02
shin- <joffrey@dotcloud.com>
2013-09-14 00:56:34 +0200

    Bumped version in setup.py and changed development status from alpha to beta

--- setup.py
+++ setup.py
@@ -8,16 +8,18 @@ SOURCE_DIR = os.path.join(ROOT_DIR)
 test_requirements = []
 setup(
     name="docker-py",
-    version='0.1.5',
+    version='0.2.0',
     description="Python client for Docker.",
     packages=['docker'],
     install_requires=['requests', 'six'] + test_requirements,
     zip_safe=False,
     test_suite='tests',
-    classifiers=['Development Status :: 3 - Alpha',
+    classifiers=['Development Status :: 4 - Beta',
                  'Environment :: Other Environment',
                  'Intended Audience :: Developers',
                  'Operating System :: OS Independent',
                  'Programming Language :: Python',
-                 'Topic :: Utilities'],
+                 'Topic :: Utilities',
+                 'License :: OSI Approved :: Apache Software License'
+],
     )
