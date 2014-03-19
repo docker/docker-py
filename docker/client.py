e8b993ab8ad687febda232c9e55e553b0a6f5a74
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-03-19 08:43:13 -0700

    Use requests to read chunk data instead of parsing the raw stream
    The previous code had a bug in which it assumed that the chunk data had
    no newlines in it.  In 0.9.0 newlines were added to the stream results,
    which exposed this bug.

--- docker/client.py
+++ docker/client.py
@@ -233,17 +233,8 @@ class Client(requests.Session):
 
     def _stream_helper(self, response):
         """Generator for data coming from a chunked-encoded HTTP response."""
-        socket_fp = self._get_raw_response_socket(response)
-        socket_fp.setblocking(1)
-        socket = socket_fp.makefile()
-        while True:
-            size = int(socket.readline(), 16)
-            if size <= 0:
-                break
-            data = socket.readline()
-            if not data:
-                break
-            yield data
+        for line in response.iter_lines(chunk_size=32):
+            yield line
 
     def _multiplexed_buffer_helper(self, response):
         """A generator of multiplexed data blocks read from a buffered
