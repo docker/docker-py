74499e21e426b4ff6a89b9a1f2fc72582675ddcd
Luka Stojanovic <luka@magrathea.rs>
2014-03-24 16:08:44 +0100

    make flake happy

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -6,12 +6,18 @@ from docker.utils import parse_repository_tag
 class UtilsTest(unittest.TestCase):
 
     def test_parse_repository_tag(self):
-        self.assertEqual(parse_repository_tag("root"), ("root", ""))
-        self.assertEqual(parse_repository_tag("root:tag"), ("root", "tag"))
-        self.assertEqual(parse_repository_tag("user/repo"), ("user/repo", ""))
-        self.assertEqual(parse_repository_tag("user/repo:tag"), ("user/repo", "tag"))
-        self.assertEqual(parse_repository_tag("url:5000/repo"), ("url:5000/repo", ""))
-        self.assertEqual(parse_repository_tag("url:5000/repo:tag"), ("url:5000/repo", "tag"))
+        self.assertEqual(parse_repository_tag("root"),
+                         ("root", ""))
+        self.assertEqual(parse_repository_tag("root:tag"),
+                         ("root", "tag"))
+        self.assertEqual(parse_repository_tag("user/repo"),
+                         ("user/repo", ""))
+        self.assertEqual(parse_repository_tag("user/repo:tag"),
+                         ("user/repo", "tag"))
+        self.assertEqual(parse_repository_tag("url:5000/repo"),
+                         ("url:5000/repo", ""))
+        self.assertEqual(parse_repository_tag("url:5000/repo:tag"),
+                         ("url:5000/repo", "tag"))
 
 
 if __name__ == '__main__':
