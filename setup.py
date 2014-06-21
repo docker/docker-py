dc1020fa1138d1f287503fa4a8fb5a49e6a0b75a
Joffrey F <joffrey@docker.com>
2014-06-21 02:19:56 +0200

    Added support for Python 3.4 in setup.py classifiers

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Utilities',
         'License :: OSI Approved :: Apache Software License',
     ],
