1756496858ec358b4274eb421d4c9ef5b034395b
micahhausler <micah.hausler@ambition.com>
2014-10-27 16:46:47 -0400

    Fixed streaming for py3

--- docker/client.py
+++ docker/client.py
@@ -17,7 +17,6 @@ import os
 import re
 import shlex
 import struct
-from socket import socket as socket_obj
 import warnings
 
 import requests
@@ -276,23 +275,9 @@ class Client(requests.Session):
 
     def _stream_helper(self, response):
         """Generator for data coming from a chunked-encoded HTTP response."""
-        socket_fp = socket_obj(_sock=self._get_raw_response_socket(response))
-        socket_fp.setblocking(1)
-        socket = socket_fp.makefile()
-        while True:
-            # Because Docker introduced newlines at the end of chunks in v0.9,
-            # and only on some API endpoints, we have to cater for both cases.
-            size_line = socket.readline()
-            if size_line == '\r\n' or size_line == '\n':
-                size_line = socket.readline()
-
-            size = int(size_line, 16)
-            if size <= 0:
-                break
-            data = socket.readline()
-            if not data:
-                break
-            yield data
+        for line in response.iter_lines():
+            if line:
+                yield line
 
     def _multiplexed_buffer_helper(self, response):
         """A generator of multiplexed data blocks read from a buffered
