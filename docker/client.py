93cb8fd2f9d38b13f2b44dd8e7cd37766cf9dd98
Andrew Glen-Young <andrew@wot.io>
2014-11-09 20:44:26 +0000

    Ensure client version is a string.

--- docker/client.py
+++ docker/client.py
@@ -51,7 +51,7 @@ class Client(requests.Session):
                 'If using TLS, the base_url argument must begin with '
                 '"https://".')
         self.base_url = base_url
-        self._version = version
+        self._version = str(version)
         self._timeout = timeout
         self._auth_configs = auth.load_config()
 
