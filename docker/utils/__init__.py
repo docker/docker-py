e2233fcee11e5d74b1849e502c78cc2eca841a7e
Joffrey F <joffrey@docker.com>
2015-02-11 14:02:21 -0800

    * Moved _container_config to utils.create_container_config * memswap_limit can now be provided as a string, similar to mem_limit

--- docker/utils/__init__.py
+++ docker/utils/__init__.py
@@ -1,5 +1,6 @@
 from .utils import (
     compare_version, convert_port_bindings, convert_volume_binds,
     mkbuildcontext, ping, tar, parse_repository_tag, parse_host,
-    kwargs_from_env, convert_filters, create_host_config
+    kwargs_from_env, convert_filters, create_host_config,
+    create_container_config, parse_bytes
 ) # flake8: noqa
