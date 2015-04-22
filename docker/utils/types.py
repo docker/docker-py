d87577c7c6ad272284a3e1c7032af19a0491910d
Joffrey F <joffrey@docker.com>
2015-04-22 15:45:38 -0700

    improved Ulimit class

--- docker/utils/types.py
+++ docker/utils/types.py
@@ -8,7 +8,10 @@ class DictType(dict):
 
 
 class Ulimit(DictType):
-    def __init__(self, name, soft=None, hard=None):
+    def __init__(self, **kwargs):
+        name = kwargs.get('name', kwargs.get('Name'))
+        soft = kwargs.get('soft', kwargs.get('Soft'))
+        hard = kwargs.get('hard', kwargs.get('Hard'))
         if not isinstance(name, six.string_types):
             raise ValueError("Ulimit.name must be a string")
         if soft and not isinstance(soft, int):
@@ -16,19 +19,31 @@ class Ulimit(DictType):
         if hard and not isinstance(hard, int):
             raise ValueError("Ulimit.hard must be an integer")
         super(Ulimit, self).__init__({
-            'name': name,
-            'soft': soft,
-            'hard': hard
+            'Name': name,
+            'Soft': soft,
+            'Hard': hard
         })
 
     @property
     def name(self):
-        return self['name']
+        return self['Name']
+
+    @name.setter
+    def name(self, value):
+        self['Name'] = value
 
     @property
     def soft(self):
-        return self.get('soft')
+        return self.get('Soft')
+
+    @soft.setter
+    def soft(self, value):
+        self['Soft'] = value
 
     @property
     def hard(self):
-        return self.get('hard')
+        return self.get('Hard')
+
+    @hard.setter
+    def hard(self, value):
+        self['Hard'] = value
