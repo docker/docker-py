abbbbfb2d6a1294238919aa115631416175f1dde
Joffrey F <joffrey@docker.com>
2015-05-20 14:58:47 -0700

    Only allow privileged exec_create for API version >= 1.19

--- docker/client.py
+++ docker/client.py
@@ -531,10 +531,14 @@ class Client(requests.Session):
 
         return self.exec_start(create_res, detach, tty, stream)
 
-    def exec_create(self, container, cmd, stdout=True,
-                    stderr=True, tty=False, privileged=False):
+    def exec_create(self, container, cmd, stdout=True, stderr=True, tty=False,
+                    privileged=False):
         if utils.compare_version('1.15', self._version) < 0:
             raise errors.InvalidVersion('Exec is not supported in API < 1.15')
+        if privileged and utils.compare_version('1.19', self._version) < 0:
+            raise errors.InvalidVersion(
+                'Privileged exec is not supported in API < 1.19'
+            )
         if isinstance(container, dict):
             container = container.get('Id')
         if isinstance(cmd, six.string_types):
