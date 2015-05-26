5a4a0941aa6b711dc65c82b38d85f1c847ae89e2
Joffrey F <joffrey@docker.com>
2015-05-26 16:16:22 -0700

    Bumped version (rc)

--- docker/version.py
+++ docker/version.py
@@ -1,2 +1,2 @@
-version = "1.2.3-dev"
-version_info = tuple([int(d) for d in version.replace("-dev", "").split(".")])
+version = "1.2.3-rc0"
+version_info = tuple([int(d) for d in version.split("-")[0].split(".")])
