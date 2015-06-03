b10e6d51a2db199bdd2f05aa49e37b388c40ae0d
Luke Marsden <luke@clusterhq.com>
2015-06-03 12:00:43 +0100

    Add volume_driver param to client.create_container
    - Add appropriate test which also asserts that volume names can be passed through to drivers.
    - Add new param to docs.
    
    Signed-off-by: Luke Marsden <luke@clusterhq.com>

--- docker/utils/utils.py
+++ docker/utils/utils.py
@@ -489,7 +489,7 @@ def create_container_config(
     dns=None, volumes=None, volumes_from=None, network_disabled=False,
     entrypoint=None, cpu_shares=None, working_dir=None, domainname=None,
     memswap_limit=0, cpuset=None, host_config=None, mac_address=None,
-    labels=None
+    labels=None, volume_driver=None
 ):
     if isinstance(command, six.string_types):
         command = shlex.split(str(command))
@@ -589,4 +589,5 @@ def create_container_config(
         'HostConfig': host_config,
         'MacAddress': mac_address,
         'Labels': labels,
+        'VolumeDriver': volume_driver,
     }
