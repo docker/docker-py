427e3a60232a5b8ba6c2b91005908321366a0113
Joffrey F <joffrey@docker.com>
2015-06-19 02:12:32 +0200

    Moved mem_limit and memswap_limit to host_config for API version >= 1.19

--- docker/client.py
+++ docker/client.py
@@ -464,11 +464,11 @@ class Client(requests.Session):
 
     def create_container(self, image, command=None, hostname=None, user=None,
                          detach=False, stdin_open=False, tty=False,
-                         mem_limit=0, ports=None, environment=None, dns=None,
-                         volumes=None, volumes_from=None,
+                         mem_limit=None, ports=None, environment=None,
+                         dns=None, volumes=None, volumes_from=None,
                          network_disabled=False, name=None, entrypoint=None,
                          cpu_shares=None, working_dir=None, domainname=None,
-                         memswap_limit=0, cpuset=None, host_config=None,
+                         memswap_limit=None, cpuset=None, host_config=None,
                          mac_address=None, labels=None, volume_driver=None):
 
         if isinstance(volumes, six.string_types):
