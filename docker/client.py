33e1a58b6034f4b586a55b5b42e644c11c281d25
Aanand Prasad <aanand.prasad@gmail.com>
2015-07-20 09:36:35 +0100

    Stop pinging registries from the client
    The daemon already pings the registry, so doing it on our end is
    redundant and error-prone.
    
    The `insecure_registry` argument to `push()`, `pull()` and `login()` has
    been deprecated - in the latter case, it wasn't being used anyway.
    
    The `insecure` argument to `docker.auth.resolve_repository_name()` has
    also been deprecated.
    
    `docker.utils.ping_registry()` has been deprecated.
    
    `docker.auth.expand_registry_url()` has been removed.
    
    Signed-off-by: Aanand Prasad <aanand.prasad@gmail.com>

--- docker/client.py
+++ docker/client.py
@@ -25,6 +25,7 @@ from . import constants
 from . import errors
 from .auth import auth
 from .utils import utils, check_resource
+from .constants import INSECURE_REGISTRY_DEPRECATION_WARNING
 
 
 class Client(clientbase.ClientBase):
@@ -499,6 +500,12 @@ class Client(clientbase.ClientBase):
 
     def login(self, username, password=None, email=None, registry=None,
               reauth=False, insecure_registry=False, dockercfg_path=None):
+        if insecure_registry:
+            warnings.warn(
+                INSECURE_REGISTRY_DEPRECATION_WARNING.format('login()'),
+                DeprecationWarning
+            )
+
         # If we don't have any auth data so far, try reloading the config file
         # one more time in case anything showed up in there.
         # If dockercfg_path is passed check to see if the config file exists,
@@ -584,11 +591,15 @@ class Client(clientbase.ClientBase):
 
     def pull(self, repository, tag=None, stream=False,
              insecure_registry=False, auth_config=None):
+        if insecure_registry:
+            warnings.warn(
+                INSECURE_REGISTRY_DEPRECATION_WARNING.format('pull()'),
+                DeprecationWarning
+            )
+
         if not tag:
             repository, tag = utils.parse_repository_tag(repository)
-        registry, repo_name = auth.resolve_repository_name(
-            repository, insecure=insecure_registry
-        )
+        registry, repo_name = auth.resolve_repository_name(repository)
         if repo_name.count(":") == 1:
             repository, tag = repository.rsplit(":", 1)
 
@@ -631,11 +642,15 @@ class Client(clientbase.ClientBase):
 
     def push(self, repository, tag=None, stream=False,
              insecure_registry=False):
+        if insecure_registry:
+            warnings.warn(
+                INSECURE_REGISTRY_DEPRECATION_WARNING.format('push()'),
+                DeprecationWarning
+            )
+
         if not tag:
             repository, tag = utils.parse_repository_tag(repository)
-        registry, repo_name = auth.resolve_repository_name(
-            repository, insecure=insecure_registry
-        )
+        registry, repo_name = auth.resolve_repository_name(repository)
         u = self._url("/images/{0}/push".format(repository))
         params = {
             'tag': tag
