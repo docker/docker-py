b3d34824c8c1c00a596b9b57027da625efef56f7
Mike May <themistymay@gmail.com>
2015-10-09 07:23:23 -0400

    added cpusetmems as a constant and updated docs
    Signed-off-by: Mike May <themistymay@gmail.com>

--- docker/constants.py
+++ docker/constants.py
@@ -2,7 +2,7 @@ DEFAULT_DOCKER_API_VERSION = '1.20'
 DEFAULT_TIMEOUT_SECONDS = 60
 STREAM_HEADER_SIZE_BYTES = 8
 CONTAINER_LIMITS_KEYS = [
-    'memory', 'memswap', 'cpushares', 'cpusetcpus'
+    'memory', 'memswap', 'cpushares', 'cpusetcpus', 'cpusetmems'
 ]
 
 INSECURE_REGISTRY_DEPRECATION_WARNING = \
