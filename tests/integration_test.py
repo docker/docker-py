c622ff64b902770da582a391346f70d642e652b8
Joffrey F <joffrey@docker.com>
2015-11-12 11:16:43 -0800

    Remove unused files
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- tests/integration_test.py
+++
@@ -1,4 +0,0 @@
-# FIXME: placeholder while we transition to the new folder architecture
-# Remove when merged in master and Jenkins is updated to find the tests
-# in the new location.
-from .integration import * # flake8: noqa
