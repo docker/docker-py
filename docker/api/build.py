9dbccce5b68dca9e7139f4cac474bcadc4f06bb1
Michael Sander <michael.sander@gmail.com>
2016-02-09 05:29:39 -0500

    Fix line-spacing for build
    Signed-off-by: Michael Sander <michael.sander@docketalarm.com>

--- docker/api/build.py
+++ docker/api/build.py
@@ -46,7 +46,8 @@ class BuildApiMixin(object):
             if os.path.exists(dockerignore):
                 with open(dockerignore, 'r') as f:
                     exclude = list(filter(bool, f.read().splitlines()))
-            context = utils.tar(path, exclude=exclude, dockerfile=dockerfile, gzip=True)
+            context = utils.tar(path, exclude=exclude, dockerfile=dockerfile,
+                gzip=True)
             encoding = 'gizp'
 
         if utils.compare_version('1.8', self._version) >= 0:
