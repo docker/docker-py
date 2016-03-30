0a5815bcad613fe445820e9f565381fa051e0e2c
Joffrey F <joffrey@docker.com>
2016-03-29 17:08:31 -0700

    Add match_hostname implementation and monkey-patch for py<3.5
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- docker/ssladapter/ssladapter.py
+++ docker/ssladapter/ssladapter.py
@@ -2,6 +2,8 @@
       https://lukasa.co.uk/2013/01/Choosing_SSL_Version_In_Requests/
       https://github.com/kennethreitz/requests/pull/799
 """
+import sys
+
 from distutils.version import StrictVersion
 from requests.adapters import HTTPAdapter
 
@@ -10,7 +12,14 @@ try:
 except ImportError:
     import urllib3
 
+
 PoolManager = urllib3.poolmanager.PoolManager
+
+# Monkey-patching match_hostname with a version that supports
+# IP-address checking. Not necessary for Python 3.5 and above
+if sys.version_info[0] < 3 or sys.version_info[1] < 5:
+    from .ssl_match_hostname import match_hostname
+    urllib3.connection.match_hostname = match_hostname
 
 
 class SSLAdapter(HTTPAdapter):
