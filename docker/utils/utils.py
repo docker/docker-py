85c45e77ef908082f57a7e4129e141d8692775ae
Jamie Greeff <jamie@greeff.me>
2016-08-05 10:32:22 +0100

    Make client responsible for formatting healthcheck config
    Also added some new utility functions to help construct the
    healthcheck config:
    
    create_healthcheck
    create_healthcheck_test_from_command
    
    Signed-off-by: Jamie Greeff <jamie@greeff.me>

--- docker/utils/utils.py
+++ docker/utils/utils.py
@@ -10,7 +10,6 @@ import warnings
 from distutils.version import StrictVersion
 from datetime import datetime
 from fnmatch import fnmatch
-from pytimeparse.timeparse import timeparse
 
 import requests
 import six
@@ -52,6 +51,22 @@ def create_ipam_config(driver='default', pool_configs=None):
         'Driver': driver,
         'Config': pool_configs or []
     }
+
+
+def create_healthcheck(test=None, interval=None, timeout=None, retries=None):
+    return {
+        'Test': test,
+        'Interval': interval,
+        'Timeout': timeout,
+        'Retries': retries,
+    }
+
+
+def create_healthcheck_test_from_command(command=None):
+    return [
+        'CMD-SHELL',
+        command
+    ]
 
 
 def mkbuildcontext(dockerfile):
@@ -990,8 +1005,6 @@ def format_environment(environment):
         return u'{key}={value}'.format(key=key, value=value)
     return [format_env(*var) for var in six.iteritems(environment)]
 
-def duration_from_string(duration_string):
-    return timeparse(duration_string) * 1000000000
 
 def create_container_config(
     version, image, command, hostname=None, user=None, detach=False,
@@ -1082,17 +1095,6 @@ def create_container_config(
         for vol in volumes:
             volumes_dict[vol] = {}
         volumes = volumes_dict
-
-    if isinstance(healthcheck, dict):
-        healthcheck = {
-            'Test': [
-                'CMD-SHELL',
-                healthcheck.get('command')
-            ],
-            'Interval': duration_from_string(healthcheck.get('interval')) if healthcheck.has_key('interval') else None,
-            'Timeout': duration_from_string(healthcheck.get('timeout')) if healthcheck.has_key('timeout') else None,
-            'Retries': healthcheck.get('retries')
-        }
 
     if volumes_from:
         if not isinstance(volumes_from, six.string_types):
