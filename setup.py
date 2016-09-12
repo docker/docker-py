abe66e83f3f145425521b529b475453c93e0913c
David Diaz <dajose22@gmail.com>
2016-09-12 11:28:23 -0600

    Upgrade requests to latest version
    Signed-off-by: David Diaz <dajose22@gmail.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
 
 requirements = [
-    'requests >= 2.5.2, < 2.11',
+    'requests >= 2.5.2',
     'six >= 1.4.0',
     'websocket-client >= 0.32.0',
     'docker-pycreds >= 0.2.1'
