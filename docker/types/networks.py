4669b24b466002aa3a81397d194ed00b0350c0a8
Joffrey F <joffrey@docker.com>
2016-12-02 15:04:05 -0800

    Add options to IPAMConfig
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- docker/types/networks.py
+++ docker/types/networks.py
@@ -50,6 +50,8 @@ class IPAMConfig(dict):
         driver (str): The IPAM driver to use. Defaults to ``default``.
         pool_configs (list): A list of pool configurations
           (:py:class:`~docker.types.IPAMPool`). Defaults to empty list.
+        options (dict): Driver options as a key-value dictionary.
+          Defaults to `None`.
 
     Example:
 
@@ -57,11 +59,16 @@ class IPAMConfig(dict):
         >>> network = client.create_network('network1', ipam=ipam_config)
 
     """
-    def __init__(self, driver='default', pool_configs=None):
+    def __init__(self, driver='default', pool_configs=None, options=None):
         self.update({
             'Driver': driver,
             'Config': pool_configs or []
         })
+
+        if options:
+            if not isinstance(options, dict):
+                raise TypeError('IPAMConfig options must be a dictionary')
+            self['Options'] = options
 
 
 class IPAMPool(dict):
