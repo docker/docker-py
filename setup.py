e9691db91bc1c25999185b055afa62c4a9ee3265
Joffrey F <joffrey@docker.com>
2017-01-18 16:26:22 -0800

    Prevent issues when installing docker and docker-py in the same environment
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 import codecs
 import os
 import sys
 
+import pip
+
 from setuptools import setup, find_packages
 
+if 'docker-py' in [x.project_name for x in pip.get_installed_distributions()]:
+    print(
+        'ERROR: "docker-py" needs to be uninstalled before installing this'
+        ' package:\npip uninstall docker-py', file=sys.stderr
+    )
+    sys.exit(1)
 
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
