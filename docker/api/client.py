ff993dd858ffb3c6367013ed2c468903f0cf4fe9
grahamlyons <graham@grahamlyons.com>
2017-06-09 09:47:00 +0100

    Move default `timeout` into `from_env`
    We'd like to be able to pass `None` as a value for `timeout` because
    it has meaning to the `requests` library
    (http://docs.python-requests.org/en/master/user/advanced/#timeouts)
    
    Signed-off-by: grahamlyons <graham@grahamlyons.com>

--- docker/api/client.py
+++ docker/api/client.py
@@ -83,7 +83,8 @@ class APIClient(
             configuration.
         user_agent (str): Set a custom user agent for requests to the server.
     """
-    def __init__(self, base_url=None, version=None, timeout=None, tls=False,
+    def __init__(self, base_url=None, version=None,
+                 timeout=DEFAULT_TIMEOUT_SECONDS, tls=False,
                  user_agent=DEFAULT_USER_AGENT, num_pools=DEFAULT_NUM_POOLS):
         super(APIClient, self).__init__()
 
@@ -93,11 +94,7 @@ class APIClient(
             )
 
         self.base_url = base_url
-        if timeout is not None:
-            self.timeout = timeout
-        else:
-            self.timeout = DEFAULT_TIMEOUT_SECONDS
-
+        self.timeout = timeout
         self.headers['User-Agent'] = user_agent
 
         self._auth_configs = auth.load_config()
