80efa204570ce6de64142f1558e818f901e72f1d
Joffrey F <joffrey@docker.com>
2017-11-06 16:04:00 -0800

    Add support for insert_defaults in inspect_service
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- docker/api/service.py
+++ docker/api/service.py
@@ -136,12 +136,14 @@ class ServiceApiMixin(object):
 
     @utils.minimum_version('1.24')
     @utils.check_resource('service')
-    def inspect_service(self, service):
+    def inspect_service(self, service, insert_defaults=None):
         """
         Return information about a service.
 
         Args:
-            service (str): Service name or ID
+            service (str): Service name or ID.
+            insert_defaults (boolean): If true, default values will be merged
+                into the service inspect output.
 
         Returns:
             ``True`` if successful.
@@ -151,7 +153,15 @@ class ServiceApiMixin(object):
                 If the server returns an error.
         """
         url = self._url('/services/{0}', service)
-        return self._result(self._get(url), True)
+        params = {}
+        if insert_defaults is not None:
+            if utils.version_lt(self._version, '1.29'):
+                raise errors.InvalidVersion(
+                    'insert_defaults is not supported in API version < 1.29'
+                )
+            params['insertDefaults'] = insert_defaults
+
+        return self._result(self._get(url, params=params), True)
 
     @utils.minimum_version('1.24')
     @utils.check_resource('task')
