f965ad9d695e698606bdeafba8406d573f64c388
Ryan Konkul <rkonkul@gmail.com>
2018-06-06 17:04:43 -0500

    Fix windows path building
    Signed-off-by: Ryan Konkul <rkonkul@gmail.com>

--- docker/utils/build.py
+++ docker/utils/build.py
@@ -138,7 +138,7 @@ def build_file_list(root):
         for filename in fnames + dirnames:
             longpath = os.path.join(dirname, filename)
             files.append(
-                longpath.replace(root, '', 1).lstrip('/')
+                longpath.replace(root, '', 1).lstrip('/').lstrip('\\')
             )
 
     return files
