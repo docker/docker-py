3b53a057bcb4685e84bb85c141275c84cfc35280
andrew000 <slavaukraini125@gmail.com>
2019-12-04 04:14:03 +0200

    Update __init__.py
    Signed-off-by: root <andrew000@githun.com>

--- docker/__init__.py
+++ docker/__init__.py
@@ -2,6 +2,7 @@
 from .api import APIClient
 from .client import DockerClient, from_env
 from .version import version, version_info
+from .errors import *
 
 __version__ = version
 __title__ = 'docker'
