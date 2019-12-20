32d2e3bf41d48200bea1ebd040152d936d8d1a69
Erwan Rouchet <rouchet@teklia.com>
2019-12-20 10:10:03 +0100

    Use parentheses for multiline import
    Signed-off-by: Erwan Rouchet <rouchet@teklia.com>

--- docker/types/__init__.py
+++ docker/types/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
-from .containers import \
+from .containers import (
     ContainerConfig, HostConfig, LogConfig, Ulimit, DeviceRequest
+)
 from .daemon import CancellableStream
 from .healthcheck import Healthcheck
 from .networks import EndpointConfig, IPAMConfig, IPAMPool, NetworkingConfig
