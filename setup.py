3e62517c61e78f5956d8c457050951807bc2a0b7
Christian Clauss <cclauss@me.com>
2020-12-24 15:14:05 +0100

    setup.py: Add support for Python 3.8 and 3.9
    Signed-off-by: Christian Clauss <cclauss@me.com>

--- setup.py
+++ setup.py
@@ -84,6 +84,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development',
         'Topic :: Utilities',
         'License :: OSI Approved :: Apache Software License',
