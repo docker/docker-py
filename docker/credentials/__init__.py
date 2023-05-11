fd925d59590f87909c1d70e22b35c0616fe72ccd
Aarni Koskela <akx@iki.fi>
2023-05-11 15:50:13 +0300

    Get rid of star import
    Signed-off-by: Aarni Koskela <akx@iki.fi>

--- docker/credentials/__init__.py
+++ docker/credentials/__init__.py
@@ -1,4 +1,8 @@
-
 from .store import Store
 from .errors import StoreError, CredentialsNotFound
-from .constants import *  # noqa: F403
+from .constants import (
+    DEFAULT_LINUX_STORE,
+    DEFAULT_OSX_STORE,
+    DEFAULT_WIN32_STORE,
+    PROGRAM_PREFIX,
+)
