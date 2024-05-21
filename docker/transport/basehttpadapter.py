d8e9bcb2780607faf388f8832bff3865eb24dce0
Felix Fontein <felix@fontein.de>
2024-05-21 21:05:36 +0200

    requests 2.32.0 and 2.32.1 have been yanked.
    Signed-off-by: Felix Fontein <felix@fontein.de>

--- docker/transport/basehttpadapter.py
+++ docker/transport/basehttpadapter.py
@@ -7,13 +7,6 @@ class BaseHTTPAdapter(requests.adapters.HTTPAdapter):
         if hasattr(self, 'pools'):
             self.pools.clear()
 
-    # Hotfix for requests 2.32.0 and 2.32.1: its commit
-    # https://github.com/psf/requests/commit/c0813a2d910ea6b4f8438b91d315b8d181302356
-    # changes requests.adapters.HTTPAdapter to no longer call get_connection() from
-    # send(), but instead call _get_connection().
-    def _get_connection(self, request, *args, proxies=None, **kwargs):
-        return self.get_connection(request.url, proxies)
-
     # Fix for requests 2.32.2+:
     # https://github.com/psf/requests/commit/c98e4d133ef29c46a9b68cd783087218a8075e05
     def get_connection_with_tls_context(self, request, verify, proxies=None, cert=None):
