104c5c45f4da15a1d5e0b0d28a0f01210772ad47
Christopher Petito <47751006+krissetto@users.noreply.github.com>
2024-05-22 09:54:56 +0000

    Hotfix for requests 2.32.2
    Signed-off-by: Christopher Petito <47751006+krissetto@users.noreply.github.com>

--- docker/transport/basehttpadapter.py
+++ docker/transport/basehttpadapter.py
@@ -6,3 +6,8 @@ class BaseHTTPAdapter(requests.adapters.HTTPAdapter):
         super().close()
         if hasattr(self, 'pools'):
             self.pools.clear()
+
+    # Fix for requests 2.32.2+:
+    # https://github.com/psf/requests/commit/c98e4d133ef29c46a9b68cd783087218a8075e05
+    def get_connection_with_tls_context(self, request, verify, proxies=None, cert=None):
+        return self.get_connection(request.url, proxies)
