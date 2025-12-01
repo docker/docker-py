9f7af3d3e927525e75c334339f073f3d063136ba
kasium <15907922+kasium@users.noreply.github.com>
2025-12-01 13:17:00 +0100

    Improve tmpfs_mode docs
    I had recently a great time to find out, why the tmpfs_mode=777 does not work until I've found out that in needs to be in octal. Therefore I created this PR for clarity
    
    Signed-off-by: kasium <15907922+kasium@users.noreply.github.com>

--- docker/types/services.py
+++ docker/types/services.py
@@ -244,7 +244,7 @@ class Mount(dict):
           for the ``volume`` type.
         subpath (str): Path inside a volume to mount instead of the volume root.
         tmpfs_size (int or string): The size for the tmpfs mount in bytes.
-        tmpfs_mode (int): The permission mode for the tmpfs mount.
+        tmpfs_mode (int): The permission mode in octal for the tmpfs mount (e.g. ``0o777``).
     """
 
     def __init__(self, target, source, type='volume', read_only=False,
